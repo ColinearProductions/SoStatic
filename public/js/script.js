1f1e64727fa07c531f13c2a9eddb969d195996e9
Colinear <becheru.razvan@gmail.com>
2018-02-03 23:31:57 +0200

    Individual Website page

--- public/js/script.js
+++ public/js/script.js
@@ -73,3 +73,10 @@ $(".smooth_scroll").on('click', function (event) {
 });
 
 
+
+
+
+$(document).ready(function () {
+    // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
+    $('.modal').modal();
+});
