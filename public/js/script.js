d4abaf160c1273ecdfb884d772be6cfe8675bbd5
Colinear <becheru.razvan@gmail.com>
2018-02-04 21:49:56 +0200

    Most of the UI done

--- public/js/script.js
+++ public/js/script.js
@@ -75,8 +75,29 @@ $(".smooth_scroll").on('click', function (event) {
 
 
 
-
 $(document).ready(function () {
     // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
     $('.modal').modal();
+    $('select').material_select();
+
+    $(".mh1").matchHeight({
+        property: 'min-height'
+    });
+
+    $(".button-collapse").sideNav();
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15, // Creates a dropdown of 15 years to control year,
+        today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+
 });
+
+
+function matchHeightUpdate() {
+    $.fn.matchHeight._update()
+}
